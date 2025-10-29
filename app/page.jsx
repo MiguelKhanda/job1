ec60343c506edb48e3567b27859e281a4b8e3972
MiguelKhanda <miguelkhanda84@gmail.com>
2025-10-29 17:30:35 +0300

    Initial commit

--- app/page.jsx
+++ app/page.jsx
@@ -9,8 +9,6 @@ import Services from "@/components/Services";
 export default function Home() {
   return (
     <div>
-      <div className="fixed inset-0 -z-12 h-full w-full items-center px-5 py-24 [background:radial-gradient(145%_125%_at_50%_30%,#252627_40%,#6BD425_100%)]">
-      </div>
       <Navbar/>
       <Hero/>
       <About/>
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,33 +7,35 @@ import { motion } from "framer-motion"
 const Hero = () => {
     const MotionImage = motion.create(Image)
   return (
-    <section className="flex items-center flex-col text-light w-[90%] mx-auto h-screen">
-        <motion.h1 
-         initial={{y:-100, scale:0}}
-         animate={{y:0, scale:1}}
-         transition={{duration:10,type:'spring', stiffness:100}}
-         className="mt-40 text-light text-4xl"><span className=" text-primary">
-            Eden</span>Care
-        </motion.h1>
-        <MotionImage
+    <section className=" bg-dark">
+        <div className="flex items-center flex-col text-light w-[90%] mx-auto h-screen">
+            <motion.h1 
             initial={{y:-100, scale:0}}
             animate={{y:0, scale:1}}
-            transition={{duration:10,type:'spring', stiffness:100, ease:'easeInOut'}}
-            src= '/images/background_home.png'
-            width={80}
-            height={80}
-            alt='Ilustration'
-            className="w-90 mb-15 shadow-2xl shadow-accent rounded-full px-2 py-5"
-            loading="eager"
-        />
-        <motion.p 
-         initial={{y:30, scale:0}}
-         animate={{y:0, scale:1}}
-         transition={{duration:2}}
-         className="text-center text-xl bg-linear-to-r from-light to-primary bg-clip-text text-transparent">
-            EdenCare is a bridge. Education, the environment, agriculture, weather, and society. All in one place. 
-        </motion.p>
-        <Button text='Consult'/>
+            transition={{duration:10,type:'spring', stiffness:100}}
+            className="mt-40 text-light text-4xl"><span className=" text-primary">
+                Eden</span>Care
+            </motion.h1>
+            <MotionImage
+                initial={{y:-100, scale:0}}
+                animate={{y:0, scale:1}}
+                transition={{duration:10,type:'spring', stiffness:100, ease:'easeInOut'}}
+                src= '/images/background_home.png'
+                width={80}
+                height={80}
+                alt='Ilustration'
+                className="w-90 mb-15 shadow-2xl shadow-accent rounded-full px-2 py-5"
+                loading="eager"
+            />
+            <motion.p 
+            initial={{y:30, scale:0}}
+            animate={{y:0, scale:1}}
+            transition={{duration:2}}
+            className="text-center text-xl bg-linear-to-r from-light to-primary bg-clip-text text-transparent">
+                EdenCare is a bridge. Education, the environment, agriculture, weather, and society. All in one place. 
+            </motion.p>
+            <Button text='Consult'/>
+        </div>
     </section>
 )
 }
